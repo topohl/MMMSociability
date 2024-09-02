08f226e1c4b6c0fadc01bb2f9b612ce53957de74
Tobias Pohl <119701302+topohl@users.noreply.github.com>
2024-09-02 17:46:53 +0200

    Update E9_SIS_AnimalPos-preprocessing.R
    ++ improved commenting and smaller clean up

--- E9_SIS_AnimalPos-preprocessing.R
+++ E9_SIS_AnimalPos-preprocessing.R
@@ -1,130 +1,140 @@
 ## 12/2023
 ## Anja Magister
-## ANALYSIS OF ANIMAL POSITIONS -PREPROCESSING ##
-##
+## ANALYSIS OF ANIMAL POSITIONS - PREPROCESSING ##
+
 ## NEEDED FILE STRUCTURE IN WORKING DIRECTORY
 ## - the data to be preprocessed must be in a folder called "raw_data"
 ## - there must be a folder called "preprocessed_data" so that this code can save its result there
-##
-## CUSTOMISABLE VARIABLES:
-## - working directory: choose your current directory
-## - batch and cage change: on which file are you working->filename will be created with batch and cc information
 
-# libraries
-library(readr)        # load readr package for reading csv files
-library(stringr)      #for splitting strings in tibble
-library(dplyr)
-library(lubridate)    # for rounding time, time operations in general
-library(tibble)       #important for tibble operations
-library(writexl)      #save as csv file
+## Load required packages
+required_packages <- c("readr", "stringr", "dplyr", "lubridate", "tibble", "writexl", "foreach", "doParallel", "parallel", "tidyverse")
 
-# paths
-working_directory <- "S:/Lab_Member/Anja/Git/MDC_Bachelor/E9_SIS_AnimalPos"
-#working_directory <- "/home/anja/Dokumente/FU BERLIN/BA/Git/MDC_Bachelor/E9_SIS_AnimalPos"
+# Install required packages if not already installed
+for (package in required_packages) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package)
+  } 
+  library(package, character.only = TRUE)
+}
 
+# Set paths
+working_dir <- "S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Analysis/Behavior/RFID/MMMSociability"
+source_dir <- "C:/Users/topohl/Documents/GitHub/MMMSociability/"
+output_dir <- file.path(working_dir, "preprocessed_data")
 
-# define batch and cage change
+# Define batch and cage change
 batches <- c("B1", "B2", "B3", "B4", "B5", "B6")
 cageChanges <- c("CC1", "CC2", "CC3", "CC4")
 
+# Read excluded animals
+excl_animals <- readLines(file.path(working_dir, "raw_data", "excluded_animals.csv"))
 
-#excluded animals
-excl_animals <- readLines(paste0(working_directory,"/raw_data/excluded_animals.csv"))
-
-
-for(batch in batches){
-  
-  for(cageChange in cageChanges){
+for (batch in batches) {
+  for (cageChange in cageChanges) {
     print(paste(batch, cageChange))
     
-    #current csv filename
+    # Current csv filename
     filename <- paste0("E9_SIS_", batch, "_", cageChange, "_AnimalPos")
     
-    #path of csv file 
-    csvFilePath <-  paste0(working_directory,"/raw_data", "/", batch, "/", filename, ".csv")
+    # Path of csv file 
+    csvFilePath <-  paste0(working_dir, "/raw_data", "/", batch, "/", filename, ".csv")
     
-    #functions
-    source(paste0(working_directory,"/E9_SIS_AnimalPos-functions.R"))
+    # Load functions
+    source(paste0(source_dir, "E9_SIS_AnimalPos-functions.R"))
     
-    # read csv file in tibble
-    overallData <- as_tibble(read_delim(csvFilePath,delim = ";", show_col_types = FALSE))
+    # Read csv file into tibble
+    data <- as_tibble(read_delim(csvFilePath, delim = ";", show_col_types = FALSE))
     
     ####################################################################################################################################
-    ###### PREPROCESSING OF overallData: ######
-    
-    # delete unnecessary columns
-    overallData <- select(overallData, -c(RFID, AM, zPos))
-    # convert the DateTime column to a datetime format(also rounds the DateTime)
-    # timezone is very important! here in utc, but we are in utc+1. UTC is important for write_csv...
-    overallData$DateTime <- as.POSIXct(overallData$DateTime, format = "%d.%m.%Y %H:%M:%S", tz="UTC")
-    
-    # separate Animal into his ID an his system
-    overallData[c('AnimalID', 'System')] <- str_split_fixed(overallData$Animal, '[_-]', 2)
-    
-    
-    
-    
-    ###### convert xPos and yPos into one column named "PositionID" ######
-    message("translate positions")
-    #create Positions_tibble that contains every possible combination of our coordinates together with an ID
-    positions <- select(overallData, c(xPos,yPos))
+    ###### PREPROCESSING OF DATA: ######
+
+    # The columns 'RFID', 'AM', and 'zPos' are not required for the analysis and are therefore removed from the dataset.
+    data <- select(data, -c(RFID, AM, zPos))
+
+    # Convert 'DateTime' Column to POSIXct Format
+    # The 'DateTime' column is converted to a POSIXct datetime object to facilitate time-based operations.
+    # The format of the datetime is specified as "%d.%m.%Y %H:%M:%S".
+    # The timezone is set to 'UTC' to standardize the data. Note that the actual experiment may be in UTC+1, but using UTC ensures
+    # consistency, particularly when exporting the data to CSV.
+    data$DateTime <- as.POSIXct(data$DateTime, format = "%d.%m.%Y %H:%M:%S", tz = "UTC")
+
+    # Separate 'Animal' Column into 'AnimalID' and 'System'
+    # The 'Animal' column, which contains a combination of the animal's ID and the system identifier, is split into two separate
+    # columns: 'AnimalID' and 'System'. The split is based on either an underscore ('_') or hyphen ('-') delimiter.
+    data[c('AnimalID', 'System')] <- str_split_fixed(data$Animal, '[_-]', 2)
+
+    ###### Convert xPos and yPos into a Single Column Named "PositionID" ######
+    message("Merging xPos and yPos into PositionID...")
+
+    # Create a Position Mapping Table
+    # A reference table ('tblPosition') is created that maps each unique combination of xPos and yPos coordinates to a unique PositionID.
+    # This step ensures that each spatial position in the system is uniquely identified by an integer ID, simplifying downstream analyses.
+    positions <- select(data, c(xPos, yPos))
     unique_positions <- unique(positions)
-    Positions_tibble <- tibble(PositionID = c(1:8), xPos = c(0,100,200,300,0,100,200,300), yPos = c(0,0,0,0,116,116,116,116))
-    
-    # Adding column PositionID to overallData instead of two colums with x and y coordinates
-    overallData_ids <- overallData %>% rowwise() %>%
-      mutate(PositionID = find_id(xPos, yPos, Positions_tibble))
-    
-    
-    
-    ##### sort columns #####
-    overallData <- overallData_ids[c('DateTime', 'AnimalID', 'System', 'PositionID')]
-    
-    ##### delete rows of excluded animals #####
-    overallData <- overallData%>%
-      filter(!AnimalID %in% excl_animals)
-    
-    ##### sort by Date Time #####
-    overallData <- overallData%>%
-      arrange(., DateTime)
-    
-    ################################################################################################################################
-    #### add new column "Phase" ####
-    
-    message("add PhaseMarker rows")
-    # add rows with specific time when the active/inactive phases for the mice change(6h30 and 18h30)
-    # important for next step (active/inactive phase division)
-    # without the marker rows we would have slightly incorrect assignments of the time of the phases
-    overallData <- addPhaseMarkerRows(overallData)
-    
-    #### add column "phase" and define values for every row depending on the time in the row ####
-    overallData <- overallData %>%
-      rowwise() %>%
-      mutate(Phase = ifelse(format(DateTime, "%H:%M", tz="UTC") >= "18:30" | format(DateTime, "%H:%M", tz="UTC") < "06:30","Active","Inactive"))
-    
-    ### add new columns "consecActive" and "consecInactive"(consecutive), tells  us how often the phases change and at which phase we are
-    message("add Consec Columns")
-    # initialize empty columns
-    overallData <- overallData %>%
-      rowwise() %>%
-      mutate(ConsecActive = 0)%>%
-      mutate(ConsecInactive = 0)
-    
-    # edit the colums so that the counting of the phases works
-    # each start of a new phase it counts a new phase
-    # important for comparison between the phases(day per day)
-    overallData <- consecPhases(overallData)
-    
+    tblPosition <- tibble(PositionID = c(1:8), 
+                         xPos = c(0, 100, 200, 300, 0, 100, 200, 300), 
+                         yPos = c(0, 0, 0, 0, 116, 116, 116, 116))
+
+    # Assign PositionID to Each Row in the Dataset
+    # The dataset is augmented with a new column 'PositionID', which replaces the original xPos and yPos coordinates.
+    # The 'find_id' function maps each (xPos, yPos) pair to its corresponding PositionID from 'tblPosition'.
+    data_ids <- data %>% rowwise() %>% mutate(PositionID = find_id(xPos, yPos, tblPosition))
+
+    # Reorder Columns for Consistency
+    # The columns are reordered to follow a logical sequence: 'DateTime', 'AnimalID', 'System', 'PositionID'.
+    data <- data_ids[c('DateTime', 'AnimalID', 'System', 'PositionID')]
+
+    # Exclude Data from Certain Animals
+    # Any rows corresponding to animals listed in 'excl_animals' are removed from the dataset.
+    # This step ensures that only data from animals of interest are retained for further analysis.
+    data <- data %>% filter(!AnimalID %in% excl_animals)
+
+    # Sort Data by DateTime
+    # The dataset is sorted by the 'DateTime' column to ensure that all rows are in chronological order.
+    data <- data %>% arrange(., DateTime)
     
     ################################################################################################################################
-    #### add necessary rows for every new day ####
-    # a start time of every mouse in every cage is needed to divide data per day
-    overallData <- addDayMarkerRows(overallData)
+    #### Add new column "Phase" ####
+    
+    message("adding row for phase transition markers...")
+    
+    # Add rows with specific time when the active/inactive phases for the mice change (6h30 and 18h30)
+    # Important for next step (active/inactive phase division)
+    # Without the marker rows, we would have slightly incorrect assignments of the time of the phases
+    data <- addPhaseTransitionMarkers(data)
+    
+    #### Add column "phase" and define values for every row depending on the time in the row ####
+    data <- data %>% rowwise() %>% mutate(Phase = ifelse(format(DateTime, "%H:%M", tz = "UTC") >= "18:30" | format(DateTime, "%H:%M", tz = "UTC") < "06:30", "Active", "Inactive"))
+    
+    ### Add new columns "consecActive" and "consecInactive" (consecutive), tells us how often the phases change and at which phase we are
+    message("adding consecutive phase counters...")
+    
+    # Initialize empty columns
+    data <- data %>% rowwise() %>% mutate(ConsecActive = 0) %>% mutate(ConsecInactive = 0)
+    
+    # Edit the columns so that the counting of the phases works
+    # Each start of a new phase, it counts a new phase
+    # Important for comparison between the phases (day per day)
+    data <- consecPhases(data)
+    
+    ################################################################################################################################
+    #### Add necessary rows for every new day ####
+    
+    # A start time of every mouse in every cage is needed to divide data per day
+    data <- addDayMarkerRows(data)
     
     ################################################################################################################################
     # SAVING #
     
-    ## saving data in csv file
-    write_csv(overallData, paste0(working_directory,"/preprocessed_data/", filename, "_preprocessed.csv"))
-  }
+    ## Saving data in csv file
+    output_dir <- file.path(working_dir, "preprocessed_data")
+    
+    if (!dir.exists(output_dir)) {
+      dir.create(output_dir, recursive = TRUE)
+    }
+    
+    write_csv(data, file.path(output_dir, paste0(filename, "_preprocessed.csv")))
+    
+    message(paste("saving file at", file.path(output_dir, paste0(filename, "_preprocessed.csv"))))
+    }
 }
